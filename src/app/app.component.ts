2eb41eeca3e30adfd84f8278f12ff4e2b5a783d3
Jose Felipe Gonzalez <josefelipe25abril@gmail.com>
2021-05-17 19:23:36 -0300

    PrimeNg module added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+  nombre:string = "Jose FelipE gonzalez";
+
+  valor:number = 1000;
+
+  obj = {
+    nombre: "Felipe"
+  }
+
+  mostrarNombre() {
+    console.log(this.nombre);
+    console.log(this.valor);
+    console.log(this.obj);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Modulo personalizado
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
+
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +12,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PrimeNgModule
   ],
   providers: [],
   bootstrap: [AppComponent]
