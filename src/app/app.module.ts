0f3a906b739d6215d2ed476565f21ca14a392197
Jose Felipe Gonzalez <josefelipe25abril@gmail.com>
2021-05-18 20:34:07 -0300

    Cambiar el idioma por defecto, esto para usar las fechas y monedas de donde se defina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRouterModule } from './app-router.module';
 
@@ -6,8 +6,12 @@ import { AppRouterModule } from './app-router.module';
 import { SharedModule } from './shared/shared.module';
 import { VentasModule } from './ventas/ventas.module';
 
-
 import { AppComponent } from './app.component';
+
+// Cambiar el locale de la app
+import localEs from '@angular/common/locales/es-UY';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData( localEs );
 
 @NgModule({
   declarations: [
@@ -20,7 +24,9 @@ import { AppComponent } from './app.component';
     SharedModule,
     VentasModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-uy' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
